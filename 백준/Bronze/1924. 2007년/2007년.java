9d7b1c173b1c17450570f1c7e00697464c69f251
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2025-11-17 00:24:41 +0900

    [Bronze I] Title: 2007년, Time: 168 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -1,28 +1,32 @@
-//chatGPT의 코드
-import java.util.*;
+import java.util.Scanner;
 
-class Main {
-    public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-
-        int month = sc.nextInt();
-        int day = sc.nextInt();
-        sc.close();
-
-        // 2007년의 각 월의 일 수를 저장한 배열
-        int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-        // 2007년 1월 1일부터 입력 받은 날짜까지의 일 수 계산
-        int days = day-1;  //월요일 인덱스가 0이므로 1일 일 때, 0이 나오게 1을 빼줌
-        for (int i = 0; i < month - 1; i++) {  //month 달의 일 수에 전 달까지의 일 수를 합해 계산
-            days += daysInMonth[i];
-        }
-
-        // 2007년 1월 1일이 월요일이므로, days를 7로 나눈 나머지를 인덱스로 이용하여 요일 계산
-        int dayOfWeek = (days+1) % 7;  
-
-        String[] weekDays = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
-
-        System.out.println(weekDays[dayOfWeek]);
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		int day = y;  // 일 수를 미리 더해줌
+		
+		// 지난 개월 수에 따른 일 수 계산을 추가로 합
+		for (int i=1; i<x; i++) {
+				switch(i) {
+				case 1:
+				case 3:
+				case 5:
+				case 7:
+				case 8:
+				case 10:
+				case 12:
+					day+=31;
+					break;
+				case 2: day+=28; break;
+				default: day+=30; break;
+			}
+		}
+		
+		String[] date = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+		System.out.println(date[day%7]);
     }
 }
