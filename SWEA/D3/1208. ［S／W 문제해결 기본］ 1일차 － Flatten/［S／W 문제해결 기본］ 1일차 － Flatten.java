83170eafe76e6a384f4fc3dd94dac15c458eb1e8
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2025-11-22 04:16:37 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 105 ms, Memory: 27,508 KB -BaekjoonHub

---
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = 10;
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            st = new StringTokenizer(br.readLine());
+			int stop = Integer.parseInt(st.nextToken());
+			int[] box = new int[100];  // 상자줄은 100개
+			
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<100; i++) {
+				box[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			Arrays.sort(box);
+			
+			int dump = 0;  // 덤프 횟수
+			int min = 0 ;  // 최소값 위치
+			int max = 99;  // 최대값 위치
+			while(stop>dump) {
+				box[min]++;
+				box[max]--;
+				dump++;
+				Arrays.sort(box);
+			}
+			
+			System.out.printf("#%d %d\n", test_case, box[max]-box[min]);
+		}
+		
+	}
+}
