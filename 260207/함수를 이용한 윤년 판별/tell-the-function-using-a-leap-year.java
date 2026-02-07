b83c960d6e9627a1e5360dfc8be78cea9dbb36e4
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-07 23:54:56 +0900

    Title: 함수를 이용한 윤년 판별, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260207/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+		
+		System.out.println(isYoon(y));
+	}
+	
+	public static boolean isYoon(int year) {
+		if(year%4==0) {
+			if(year%100==0 && year%400==0) return false;
+			return true;
+		}
+		
+		return false;
+	}
+}
