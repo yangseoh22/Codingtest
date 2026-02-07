1b9b057fb127f250e60534a3d0ff642920abc115
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-07 21:54:26 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260207/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rowNum = sc.nextInt();
+        int colNum = sc.nextInt();
+        // Please write your code here.
+        rect(rowNum, colNum);
+    }
+
+    public void rect(int r, int c){
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                System.out.pirnt(1);
+            }
+            System.out.prinln();
+        }
+    }
+}
