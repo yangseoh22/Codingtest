e76d9085d599427593bf8bee596a037e537776df
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-07 23:56:41 +0900

    Title: 함수를 이용한 윤년 판별, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 260207/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
+++ 260207/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -10,7 +10,7 @@ public class Main {
 	
 	public static boolean isYoon(int year) {
 		if(year%4==0) {
-			if(year%100==0 && year%400==0) return false;
+			if(year%100==0 && year%400!=0) return false;
 			return true;
 		}
 		
