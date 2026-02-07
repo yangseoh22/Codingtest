b51f208b42852daca2da4c6cb2eba0baa1a1e039
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-07 21:50:43 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 91ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260207/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        for(int i=0; i<5; i++){
+            for(int s=0; s<10; s++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
