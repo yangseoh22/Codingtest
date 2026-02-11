49d521262441be461db4149f4173500e2889695e
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-11 15:38:34 +0900

    [Bronze I] Title: 딱지놀이, Time: 212 ms, Memory: 28656 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.java
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb  = new StringBuilder();
