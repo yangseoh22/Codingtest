58141982d6ac8031eae1bc28795944d3218a9032
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-18 20:09:24 +0900

    [D2] Title: 나무 높이, Time: 92 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	
+	static int[] tree;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -14,48 +14,36 @@ public class Solution {
 
 		for (int t = 1; t <= TC; t++) {
 			int N = Integer.parseInt(br.readLine());
-
-			// 나무 높이
-			int[] heights = new int[N];
 			
-			int maxH = Integer.MIN_VALUE;
-
+			tree = new int[N];
+			int max = 0;
+			
 			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; i++) {
-				heights[i] = Integer.parseInt(st.nextToken());
-				if (maxH < heights[i]) {  // 가장 높은 나무
-					maxH = heights[i];
-				}
+			for(int i=0; i<N; i++) {
+				tree[i] = Integer.parseInt(st.nextToken());
+				max = Math.max(max, tree[i]);
 			}
 			
 			int one = 0;  // 필요한 1일의 수
 			int two = 0;  // 필요한 2일의 수
-			
 			for(int i=0; i<N; i++) {
-				int diff = maxH - heights[i];  // 최대 높이와의 차이
-				two += diff/2;
-				one += diff%2;
+				int tmp = max - tree[i];  // 최대 길이와의 차이
+				
+				one += tmp%2;
+				two += tmp/2;
 			}
 			
-			while(true) {
-				if(one + 1>=two) break;  // 1일짜리가 많을 때는 할 수 있는 게 없음
-				else {  // 2일 짜리가 많을 때는 1일로 쪼개주기
-					two -= 1;
-					one += 2;
-				}
+			// 1일과 2일을 비슷하게 만들기
+			while(one+1<two) {
+				two--;
+				one += 2;
 			}
 			
-			// 필요한 일 수 세기
+			// 일 수 계산
 			int day = 0;
-			if(one > two) {
-				day = one *2 - 1;
-			}
-			else if(one == two) {
-				day = one + two;
-			}
-			else {
-				day = two * 2;
-			}
+			
+			if(one>two) day = one*2-1;
+			else day = two*2;
 			
 			sb.append("#").append(t).append(" ").append(day).append("\n");
 		}
