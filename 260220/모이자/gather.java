afa2db15135dfa5032428cbc210ce6d752f931a5
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-20 13:31:02 +0900

    Title: 모이자, Time: 83ms, Memory: 9MB, Status: Passed - Codetree

--- 260220/모이자/gather.java
+++ 260220/모이자/gather.java
@@ -6,7 +6,6 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(br.readLine());
 		
