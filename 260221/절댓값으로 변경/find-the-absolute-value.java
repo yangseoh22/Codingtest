09501eed03ac7b471bab4cfe15a02ed5b041065c
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-21 23:10:55 +0900

    Title: 절댓값으로 변경, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

--- 260221/절댓값으로 변경/find-the-absolute-value.java
+++ 260221/절댓값으로 변경/find-the-absolute-value.java
@@ -12,7 +12,7 @@ public class Main {
         abs(arr);
         
         for (int i = 0; i < n; i++) {
-            System.out.print(arr[i]);
+            System.out.print(arr[i] + " ");
         }
     }
 
