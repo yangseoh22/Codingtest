d599f3d65bed3ef2d8af8c0057d28eb7bfbc045f
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-21 23:10:33 +0900

    Title: 절댓값으로 변경, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260221/절댓값으로 변경/find-the-absolute-value.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        abs(arr);
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+	private static void abs(int[] arr) {
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Math.abs(arr[i]);
+		}
+	}
+}
