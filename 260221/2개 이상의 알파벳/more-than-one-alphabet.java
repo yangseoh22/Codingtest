96ba0b30005c595fdad4787943ab6dd56ba06bdf
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-21 23:17:00 +0900

    Title: 2개 이상의 알파벳, Time: 117ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260221/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+
+		System.out.println(solve(A)? "Yes":"No");
+
+	}
+
+	private static boolean solve(String A) {
+		int[] alpha = new int[26];
+		for (int i = 0; i < A.length(); i++) {
+			alpha[A.charAt(i) - 'a']++;
+		}
+		
+		int cnt = 0;
+		for (int i = 0; i < A.length(); i++) {
+			if(alpha[i]>0) cnt++;
+		}
+		
+		if(cnt>=2) return true;
+		else return false;
+	}
+}
