e6326f33464145f2584f360d26f180c0a2fa2941
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-22 01:29:00 +0900

    Title: Date to Date, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260222/Date to Date/date-to-date.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        // Please write your code here.
+        int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int before = 0;
+        int after = 0;
+
+        for(int i=0; i<m1; i++){
+            before += days[i];
+        }
+        before += d1;
+
+        for(int i=0; i<m2; i++){
+            after += days[i];
+        }
+        after += d2;
+
+        System.out.println(after - before + 1);
+        
+    }
+}
