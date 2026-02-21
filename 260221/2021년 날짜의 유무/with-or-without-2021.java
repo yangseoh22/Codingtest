f8a68722e4459d044f8575bb05f71045871a1023
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-21 17:19:13 +0900

    Title: 2021년 날짜의 유무, Time: 145ms, Memory: 10MB, Status: Passed - Codetree

--- 260221/2021년 날짜의 유무/with-or-without-2021.java
+++ 260221/2021년 날짜의 유무/with-or-without-2021.java
@@ -5,15 +5,17 @@ public class Main {
         int m = sc.nextInt();
         int d = sc.nextInt();
         
-        System.out.println(testDate(m, d)? "Yes" : "NO");
+        System.out.println(testDate(m, d)? "Yes" : "No");
 	}
 
 	private static boolean testDate(int m, int d) {
 		// 각 월의 끝나는 날
 		int[] day = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		
-		if(d>=0 && d<=day[m]) return true;
-		
+		if(m>=1 && m<=12) {
+			if(d>=0 && d<=day[m]) return true;
+		}
+
 		return false;
 	}
 }
