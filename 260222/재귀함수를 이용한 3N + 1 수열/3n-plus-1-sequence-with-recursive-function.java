a4b0674ade20049e73ebf98707346a2c953e3e7c
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-22 12:40:43 +0900

    Title: 재귀함수를 이용한 3N + 1 수열, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 260222/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.java
+++ 260222/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 public class Main {
-    static int cnt;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        solve(n);
+        int cnt = solve(n);
         
         System.out.println(cnt);
 	}
 	private static int solve(int n) {
-		if(n==1) return 1;
-		cnt++;
-		if(n%2==0) return solve(n/2);
-		else return solve(n*3 + 1);
+		if(n==1) return 0;
+
+		if(n%2==0) return solve(n/2) + 1;
+		else return solve(n*3 + 1) + 1;
 	}
 }
