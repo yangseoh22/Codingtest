8ff5952c6e1ad3729327f5dcf374f4ee6cf0aa4b
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-25 16:28:19 +0900

    [Gold V] Title: 신기한 소수, Time: 1412 ms, Memory: 11580 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static boolean[] V;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+
+		N = Integer.parseInt(br.readLine());
+
+		DFS(2, 1);
+		DFS(3, 1);
+		DFS(5, 1);
+		DFS(7, 1);
+		System.out.println(sb);
+	}
+
+	private static void DFS(int n, int jarisu) {
+
+		if (jarisu == N) {
+			if (isPrime(n))
+				sb.append(n).append("\n");
+			return;
+		}
+
+
+		// 다음 수 구하기
+		for (int i = 1; i <= 9; i += 2) {
+			int newN = n * 10 + i;
+			if (isPrime(newN)) {
+				DFS(newN, jarisu + 1);
+			}
+		}
+
+	}
+
+	// 소수 판별
+	private static boolean isPrime(int newN) {
+		for (int i = 2; i < newN; i++) {
+			if (newN % i == 0)
+				return false;
+		}
+		return true;
+	}
+
+}
