f5f431412a25f1169b07f818facb5f9acac214d0
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-02-27 00:57:25 +0900

    [Silver III] Title: N과 M (3), Time: 332 ms, Memory: 112320 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,35 +1,54 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());  //수의 범위
-        int m = Integer.parseInt(st.nextToken());  //수열의 길이
-
-        int[] tmp = new int[m];  //탐색 내용을 저장
-        backTraking(n, m, tmp, 0);
-
-        System.out.println(sb);
-    }
-    public static void backTraking(int n, int m, int[] arr, int depth) {
-        if(depth==m){
-            for(int i = 0; i < m; i++){
-                sb.append(arr[i]).append(" ");
-            }
-            sb.append('\n');
-            return;
-        }
-
-        for(int i=1; i<=n; i++){
-            arr[depth] = i;
-            backTraking(n, m, arr, depth+1);
-        }
-    }
+	static int N, M;
+	static int[] A;
+	static boolean[] V;
+	static StringBuilder sb;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		A = new int[M];
+		V = new boolean[N+1];
+		
+		perm(0);
+		
+		System.out.println(sb);
+	}
+	private static void perm(int len) {
+		if(len==M) {
+			printArr();
+			return;
+		}
+		
+		for(int i=1; i<=N; i++) {
+			A[len] = i;
+			perm(len + 1);
+		}
+		
+		// 일반 순열
+//		for(int i=1; i<=N; i++) {
+//			if(!V[i]) {
+//				A[len] = i;
+//				V[i] = true;
+//				perm(len + 1);
+//				V[i] = false;
+//			}
+//		}
+	}
+	private static void printArr() {
+		for(int a : A) {
+			sb.append(a).append(" ");
+		}
+		sb.append("\n");
+	}
+	
+	
 }
