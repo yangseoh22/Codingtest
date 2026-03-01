16fb73036b7e0a194bd932aa9bdcb95a56ab2a43
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-02 00:21:40 +0900

    Title: N개 중에 M개 뽑기, Time: 91ms, Memory: 9MB, Status: Passed - Codetree

--- 260302/N개 중에 M개 뽑기/n-choose-m.java
+++ 260302/N개 중에 M개 뽑기/n-choose-m.java
@@ -4,42 +4,37 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-		static int N, M;
-		static ArrayList<Integer> arr;
-		static StringBuilder sb;
-		static boolean[] V;
+	static int N, M;
+	static ArrayList<Integer> arr;
+	static StringBuilder sb;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		sb = new StringBuilder();
-		
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new ArrayList<>();
-		V = new boolean[N+1];
-		
+
 		// 1~N에서 M개 뽑는 조합
 		combi(0, 1);
 		System.out.println(sb);
 	}
+
 	private static void combi(int idx, int num) {
-		if(idx == M) {
-			for(int a : arr) {
+		if (idx == M) {
+			for (int a : arr) {
 				sb.append(a).append(" ");
 			}
 			sb.append("\n");
 			return;
 		}
-		
-		for(int i=num; i<=N; i++) {
-			if(!V[i]) {
-				arr.add(i);
-				V[i] = true;
-				combi(idx + 1, i + 1);
-				V[i] = false;
-				arr.remove(arr.size()-1);
-			}
-			
+
+		for (int i = num; i <= N; i++) {
+			arr.add(i);
+			combi(idx + 1, i + 1);
+			arr.remove(arr.size() - 1);
 		}
 	}
 
