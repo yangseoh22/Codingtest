abbad9a520b7da4c8442fbe7cb58586f04c364ef
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-02 01:17:40 +0900

    [Silver III] Title: N과 M (4), Time: 88 ms, Memory: 15020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,42 +1,49 @@
+/* 
+ * 전략 : 백트래킹 - 중복 조합
+ * 1. 입력 받을 것 받고, arrayList 생성
+ * 2. 조합 재귀 함수 호출 - len : 현재 선택된 수열의 숫자 수, num : 최근에 넣은 수의 다음 수
+ * 2-1. 기저 조건 :  len이 M과 같다면, 수열 출력 및 반환
+ * 2-2. num부터 N까지의 수를 수열에 추가 -> len+1, i(현재 넣은 수)로 재귀 호출
+ *  */
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M;
-	static int[] A;
+	static ArrayList<Integer> arr;
 	static StringBuilder sb;
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		sb = new StringBuilder();
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		A = new int[M];
+		N = Integer.parseInt(st.nextToken()); 
+		M = Integer.parseInt(st.nextToken()); 
 		
-		combi(1, 0);
+		arr = new ArrayList<>();
+		
+		combi(0, 1);
 		
 		System.out.println(sb);
 	}
-	private static void combi(int start, int len) {
-		if(len==M){
-			printArr();
+	
+	private static void combi(int idx, int num) {
+		if(idx == M) {
+			for(int a : arr) {
+				sb.append(a).append(" ");
+			}
+			sb.append("\n");
 			return;
 		}
 		
-		// 중복 조합이므로 썼던 거(i) 또 써도 됨
-		for(int i=start; i<=N; i++) {
-			A[len] = i;
-			combi(i, len + 1);
+		for(int i=num; i<=N; i++) {
+				arr.add(i);  // 배열은 arr[idx] = i;
+				combi(idx + 1, i);
+				arr.remove(arr.size()-1);
 		}
-	}
-	private static void printArr() {
-		for(int a : A) {
-			sb.append(a).append(" ");
-		}
-		sb.append("\n");
 	}
 
 }
