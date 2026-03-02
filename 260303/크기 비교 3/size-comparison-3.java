7dbf4dd63767105613e9dcea20c80895dbc3f7b1
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-03 00:23:05 +0900

    Title: 크기 비교 3, Time: 506ms, Memory: 27MB, Status: Passed - Codetree

--- 260303/크기 비교 3/size-comparison-3.java
+++ 260303/크기 비교 3/size-comparison-3.java
@@ -1,5 +1,3 @@
-package trail6_ch03;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -8,7 +6,7 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class lesson1_3 {
+public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
