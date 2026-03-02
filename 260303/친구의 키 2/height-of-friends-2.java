be003d53dc90f0430d566ccdcb231d952bf945c0
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-03 00:05:40 +0900

    Title: 친구의 키 2, Time: 326ms, Memory: 22MB, Status: Wrong Answer - Codetree

---
+++ 260303/친구의 키 2/height-of-friends-2.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] indegree = new int[N+1];
+		ArrayList<Integer>[] edge = new ArrayList[N+1];
+		Queue<Integer> q = new LinkedList<>();
+		for(int i=1; i<=N; i++) {
+			edge[i] = new ArrayList<>();
+		}
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			edge[a].add(b);
+			indegree[b]++;
+		}
+		
+		for(int i=1; i<=N; i++) {
+			if(indegree[i]==0)
+				q.add(i);
+		}
+		
+		int cnt = 1;
+		while(!q.isEmpty()) {
+			int now = q.poll();
+			
+			// 순서 출력
+			
+			for(int i=0; i<edge[now].size(); i++) {
+				int next = edge[now].get(i);
+				indegree[next]--;
+				
+				if(indegree[next]==0) {
+					q.add(next);
+					cnt++;
+				}
+			}
+		}
+		
+		sb.append(cnt==N? "Consistent" : "Inconsistent" );
+		
+		System.out.println(sb);
+	}
+}
