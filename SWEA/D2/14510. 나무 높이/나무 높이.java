d949c351f5f13f08b5eaf961fbc37297f017feb5
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-03 14:34:31 +0900

    [D2] Title: 나무 높이, Time: 87 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,51 +1,54 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int[] tree;
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 
 		int TC = Integer.parseInt(br.readLine());
 
-		for (int t = 1; t <= TC; t++) {
+		for (int tc = 1; tc <= TC; tc++) {
+
 			int N = Integer.parseInt(br.readLine());
-			
-			tree = new int[N];
-			int max = 0;
-			
+
+			int[] trees = new int[N];
+			int maxH = Integer.MIN_VALUE;
 			st = new StringTokenizer(br.readLine());
-			for(int i=0; i<N; i++) {
-				tree[i] = Integer.parseInt(st.nextToken());
-				max = Math.max(max, tree[i]);
+			for (int i = 0; i < N; i++) {
+				trees[i] = Integer.parseInt(st.nextToken());
+				maxH = Math.max(maxH, trees[i]); // 최대 나무 높이
 			}
 			
-			int one = 0;  // 필요한 1일의 수
-			int two = 0;  // 필요한 2일의 수
-			for(int i=0; i<N; i++) {
-				int tmp = max - tree[i];  // 최대 길이와의 차이
-				
-				one += tmp%2;
-				two += tmp/2;
+			// 최대 높이와의 차이와 필요한 2일의 수, 1일의 수 계산
+			int one = 0;
+			int two = 0;
+			for (int t : trees) {
+				int diff = maxH - t;
+
+				two += diff / 2;
+				one += diff % 2;
 			}
 			
-			// 1일과 2일을 비슷하게 만들기
-			while(one+1<two) {
+			// 둘의 차이가 1보다 크면 재분배
+			while (one+1<two) {  
 				two--;
-				one += 2;
+				one+=2;
 			}
-			
-			// 일 수 계산
+
+			// 물을 주어야 할 날 계산
 			int day = 0;
-			
-			if(one>two) day = one*2-1;
-			else day = two*2;
-			
-			sb.append("#").append(t).append(" ").append(day).append("\n");
+
+			if (one > two)
+				day = one * 2 - 1;
+			else if (one <= two)
+				day = two * 2;
+
+			sb.append("#").append(tc).append(" ").append(day).append("\n");
+
 		}
 		System.out.println(sb);
 	}
