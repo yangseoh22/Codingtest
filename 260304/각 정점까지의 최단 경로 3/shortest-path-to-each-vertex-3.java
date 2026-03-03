557a612fc265f1c29a29e03fb440b3d058a2393b
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-04 01:44:28 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 104ms, Memory: 9MB, Status: Passed - Codetree

--- 260304/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
+++ 260304/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
@@ -1,72 +1,77 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int[][] adj;
 	static boolean[] V;
 	static int[] dist;
+	static final int INF = (int) 1e9;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		
+
 		st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
-		adj = new int[N+1][N+1];
-		boolean stop = false;
-		for(int i=0; i<M; i++) {
+
+		adj = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			Arrays.fill(adj[i], INF);
+		}
+
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			int z = Integer.parseInt(st.nextToken());
-			
-			if(z < 0) {
-				stop = true;
+
+			adj[x][y] = Math.min(z, adj[x][y]);
+		}
+
+		V = new boolean[N + 1];
+		dist = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			dist[i] = (int) 1e9;
+		}
+
+		dist[1] = 0;
+
+		for (int i = 1; i <= N; i++) {
+			int minIdx = -1;
+			int minVal = INF;
+			for (int j = 1; j <= N; j++) {
+				if (!V[j] && dist[j] < minVal) {
+					minVal = dist[j];
+					minIdx = j;
+				}
+			}
+
+			if (minIdx == -1)
 				break;
-			}
-			adj[x][y] = z;
-		}
-		
-		if(stop) {
-			sb.append(-1);
-		}
-		else {
-			V = new boolean[N+1];
-			dist = new int[N + 1];
-			for(int i=1; i<=N; i++) {
-				dist[i] = (int)1e9;
-			}
-			
-			dist[1] = 0;
-			
-			for(int i=1; i<=N; i++) {
-				int minIdx = -1;
-				for(int j=1; j<=N; j++) {
-					if(V[j]) {
-						continue;
+
+			V[minIdx] = true;
+
+			for (int j = 1; j <= N; j++) {
+				if (!V[j] && adj[minIdx][j] != INF) {
+					if (dist[j] > dist[minIdx] + adj[minIdx][j]) {
+						dist[j] = dist[minIdx] + adj[minIdx][j];
 					}
-					if(minIdx==-1 || dist[minIdx]>dist[j])
-						minIdx = j;
-				}
-				
-				V[minIdx] = true;
-				
-				for(int j=1; j<=N; j++) {
-					if(adj[minIdx][j] == 0) continue;
-					
-					dist[j] = Math.min(dist[j], dist[minIdx]+adj[minIdx][j]);
 				}
 			}
-			
-			for(int i=1; i<=N; i++) {
-				if(i==1) continue;
+		}
+
+		for (int i = 2; i <= N; i++) {
+			if (dist[i] == INF) {
+				sb.append("-1\n"); // 도달할 수 없는 경우 -1 출력
+			} else {
 				sb.append(dist[i]).append("\n");
 			}
-			
 		}
+
 		System.out.println(sb);
 	}
 
