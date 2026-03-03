6eea08702a8df22fb51ecc74393c207bad5b7e62
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-03 11:40:47 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 64 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,74 +1,69 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] switches;
-	static int n;
+	static int[] code;
+	static int N;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 
-		n = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 
-		// 스위치 상태 입력 받기 - 1: 켜짐 / 0: 꺼짐
-		switches = new int[n];
+		code = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			switches[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			code[i] = Integer.parseInt(st.nextToken());
 		}
 
-		// 학생 수와 학생 성별, 받은 수
-		int std = Integer.parseInt(br.readLine());
-		for (int i = 0; i < std; i++) {
+		// 학생 수
+		int stdNum = Integer.parseInt(br.readLine());
+		for (int i = 0; i < stdNum; i++) {
 			st = new StringTokenizer(br.readLine());
-			int gen = Integer.parseInt(st.nextToken());
-			int num = Integer.parseInt(st.nextToken());
+			int nowStd = Integer.parseInt(st.nextToken());
+			int nowNum = Integer.parseInt(st.nextToken());
 
-			if (gen == 1) { // 남학생이라면
-				for (int s = 0; s < n; s++) {
-					// 스위치 번호가 num의 배수인 곳을 switch
-					if ((s + 1) % num == 0)
-						switches[s] = 1 - switches[s];
+			// 남학생
+			if (nowStd == 1) {
+				for (int m = nowNum - 1; m < N; m += nowNum) { // 받은 수 배수를 변환
+					code[m] = Math.abs(code[m] - 1);
+
 				}
-			} else if (gen == 2) { // 여학생이라면
-				// num을 기준으로 양쪽의 대칭이 맞는 곳 까지 switch
-				girlSwitch(num);
+				// sb.append("\n");
+			} else if (nowStd == 2) { // 여학생
+				switchGirl(nowNum-1); // 인덱스 접근이므로 1감소
+			}
+		}
+		
+		for (int i=0; i<N; i++) {
+            sb.append(code[i]).append(" ");
+			if((i+1)%20==0) {
+				sb.append("\n");
 			}
 		}
 
-		// 최종 스위치 상태 출력 - 20개마다 줄 바꾸기
-		for(int i=0; i<n; i++) {
-			System.out.print(switches[i] + " ");
-			if((i+1)%20==0) System.out.println();
+		System.out.println(sb);
+	}
+
+	private static void switchGirl(int n) {
+		int left = n;
+		int right = n;
+
+		while (left - 1 >= 0 && right + 1 < N) {
+			if (code[left - 1] == code[right + 1]) {
+				left--;
+				right++;
+			} else
+				break;
+
+		}
+
+		for (int i = left; i <= right; i++) {
+			code[i] = Math.abs(code[i] - 1);
 		}
 	}
 
-	private static void girlSwitch(int num) {
-		
-		int left = num-1;  // 왼쪽 스위치 번호
-		int right = num+1;  // 오른쪽 스위치 번호
-		int range = 0;  // num 기준 양 옆으로 대칭인 개수
-		
-		while(true) {
-			if(left>=1 && right<=n) {
-				if(switches[left-1] == switches[right-1]) {
-					range++;
-					left--;
-					right++;
-				}
-				else break;
-			}
-			else break;
-		}
-		
-		// range범위 switch
-		int start = num - range - 1;  // 인덱스라 1 빼주기
-		int end = num + range -1;
-		for(int i=start; i<=end; i++) {
-			switches[i] = 1 - switches[i];
-		}		
-	}
 }
