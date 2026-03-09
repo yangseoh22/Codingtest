3111feba79a0576f3d06ffec9b75ef5685b23a0d
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-09 14:12:10 +0900

    [D4] Title: 서로소 집합, Time: 414 ms, Memory: 107,088 KB -BaekjoonHub

---
+++ SWEA/D4/3289. 서로소 집합/README.md
@@ -0,0 +1,15 @@
+# [D4] 서로소 집합 - 3289 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWBJKA6qr2oDFAWr) 
+
+### 성능 요약
+
+메모리: 107,088 KB, 시간: 414 ms, 코드길이: 1,373 Bytes
+
+### 제출 일자
+
+2026-03-09 14:09
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	static int[] uf;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= T; t++) {
+			sb.append("#").append(t).append(" ");
+			
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+
+			// 서로소 집합 생성
+			uf = new int[N + 1];
+			for (int i = 1; i <= N; i++) {
+				uf[i] = i;
+			}
+
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+
+				int cmd = Integer.parseInt(st.nextToken());
+
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+
+				if (cmd == 0)
+					union(a, b);
+				else {
+					// find() 결과가 같으면 같은 집합
+					sb.append(find(a) == find(b) ? 1 : 0);
+				}
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	private static int find(int x) {
+		if (uf[x] == x)
+			return x;
+		int rootNode = find(uf[x]);
+		uf[x] = rootNode;
+		return rootNode;
+	}
+
+	private static void union(int x, int y) {
+		int rootX = find(x);
+		int rootY = find(y);
+		uf[rootX] = rootY;
+	}
+}
