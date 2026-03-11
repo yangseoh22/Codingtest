9c8eedfac6ec19b41105170bb1e7fce8d48f9923
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-11 14:41:08 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,031 ms, Memory: 218,300 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -19,9 +19,7 @@ public class Solution {
 	static int V, E;
 	static ArrayList<Edge>[] graph;
 	static boolean[] visited;
-	static int[] dist;
-	static final int INF = Integer.MAX_VALUE;
-
+	static long result;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -35,57 +33,52 @@ public class Solution {
 			E = Integer.parseInt(st.nextToken());
 
 			visited = new boolean[V + 1];
-			dist = new int[V + 1];
-
-			graph = new ArrayList[V + 1];
-			for (int i = 1; i <= V; i++) {
+			
+			graph = new ArrayList[V+1];
+			for(int i=1; i<=V; i++) {
 				graph[i] = new ArrayList<>();
-				dist[i] = INF;
 			}
-
-			for (int i = 0; i < E; i++) {
+			
+			for(int i=0; i<E; i++) {
 				st = new StringTokenizer(br.readLine());
 				int start = Integer.parseInt(st.nextToken());
 				int end = Integer.parseInt(st.nextToken());
 				int weight = Integer.parseInt(st.nextToken());
-
+				
 				graph[start].add(new Edge(end, weight));
 				graph[end].add(new Edge(start, weight));
 			}
-
-			long result = prim(1);
+			
+			prim(1);  // 임의로 1번 노드에서 시작
+			
 			sb.append("#").append(t).append(" ").append(result).append("\n");
 		}
 		System.out.println(sb);
 	}
 
-	static public long prim(int start) {
+	static public void prim(int start) {
 		PriorityQueue<Edge> pq = new PriorityQueue<>();
 		pq.offer(new Edge(start, 0));
-
-		int cnt = 0; // 연결 간선 수
-		long total = 0; // 연결 간선들의 가중치 합
-		while (!pq.isEmpty()) {
-			Edge cur = pq.poll();
-			int minIdx = cur.to;
-			int minDist = cur.w;
-
-			if (!visited[minIdx]) {
-				visited[minIdx] = true;
-				cnt++;
-				total += minDist;
-
-				for (Edge next : graph[minIdx]) {
-					if (!visited[next.to] && next.w < dist[next.to]) {
-						dist[next.to] = next.w;
-						pq.offer(new Edge(next.to, next.w));
-					}
+		
+		int cnt = 0;
+		result = 0;
+		while(!pq.isEmpty()) {
+			Edge curr = pq.poll();
+			
+			if(visited[curr.to]) continue;  // 방문했던 노드면 무시
+			
+			visited[curr.to] = true;  // 방문
+			cnt++;
+			result += curr.w;  // 선택 간선 가중치 누적 합
+			
+			// 선택한 노드의 인접 노드 확인
+			for(Edge next : graph[curr.to]) {
+				if(!visited[next.to]) {
+					pq.offer(next);
 				}
-				
-				if (cnt == V)
-				break;
 			}
+			
+			if(cnt==V) break;  // 모든 노드가 이미 선택이 끝났으면 조기 종료
 		}
-		return total;
 	}
 }
