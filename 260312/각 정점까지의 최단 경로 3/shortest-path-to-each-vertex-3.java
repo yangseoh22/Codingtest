25f6af28be1d636b5fd966a5e314ce5d6bfffc27
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-12 11:33:02 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 86ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 260312/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
+++ 260312/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
@@ -6,64 +6,58 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		
+
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
-		int[][] adj = new int[N+1][N+1];
-		for(int i=0; i<M; i++) {
+
+		int[][] adj = new int[N + 1][N + 1];
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			
+
 			adj[s][e] = w;
 		}
-		
-		int[] dist = new int[N+1];
-		for(int i=1; i<=N; i++) {
-			dist[i] = (int)1e9;
+
+		int[] dist = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			dist[i] = (int) 1e9;
 		}
 		// 시작 정점 1
 		dist[1] = 0;
-		
-		boolean[] visited = new boolean[N+1];
-		
-		for(int i=1; i<=N; i++) {
-			int minIdx=-1;
-			for(int j=1; j<=N; j++) {
-				if(visited[j]) continue;
-				
-				if(minIdx == -1 || dist[minIdx] > dist[j]) {
+
+		boolean[] visited = new boolean[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			int minIdx = -1;
+			for (int j = 1; j <= N; j++) {
+				if (visited[j])
+					continue;
+
+				if (minIdx == -1 || dist[minIdx] > dist[j]) {
 					minIdx = j;
 				}
 			}
 			visited[minIdx] = true;
-				
-			for(int j=1; j<=N; j++) {
-				if(adj[minIdx][j] == 0) continue;
-				
+
+			for (int j = 1; j <= N; j++) {
+				if (adj[minIdx][j] == 0)
+					continue;
+
 				dist[j] = Math.min(dist[j], dist[minIdx] + adj[minIdx][j]);
-			}		
-		}
-		
-		boolean isGoal = false;
-		for (int i=1; i<=N; i++){
-			if(i==1) continue;
-			
-			if(dist[i]==(int)1e9) {
-				isGoal = false;
-				break;
 			}
-			else {
-				isGoal=true;
+		}
+
+		if (dist[N] == (int) 1e9) {
+			sb.append(-1);
+		} else {
+			for (int i = 1; i <= N; i++) {
+				if (i == 1)
+					continue;
 				sb.append(dist[i]).append("\n");
 			}
 		}
-		
-		if(isGoal) {
-			System.out.println(sb);			
-		}
-		else System.out.println(-1);
+		System.out.println(sb);
 	}
 }
