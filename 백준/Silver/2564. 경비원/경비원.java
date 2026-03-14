bab9547be917c856833d7849f963ee0a91e0d064
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-14 17:16:05 +0900

    [Silver I] Title: 경비원, Time: 64 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Silver/2564. 경비원/경비원.java
+++ 백준/Silver/2564. 경비원/경비원.java
@@ -1,59 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		// 블럭의 가로와 세로
+		StringBuilder sb = new StringBuilder();
+
 		st = new StringTokenizer(br.readLine());
 		int w = Integer.parseInt(st.nextToken());
 		int h = Integer.parseInt(st.nextToken());
-		
-		// 상점 개수
-		int N = Integer.parseInt(br.readLine());
-		
-		
-		// 사각형 둘레를 한 줄로 펼친다면
-		int[] map = new int[N+1];
-		int tmp = 0;
-		for(int i=0; i<N+1; i++) {
-			// 상점의 위치와 동근이의 위치(마지막)
+
+		int n = Integer.parseInt(br.readLine());
+
+		int[] dir = new int[n+1];
+		int[] dist = new int[n+1];
+		for (int i = 0; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
-			int dir = Integer.parseInt(st.nextToken());
-			int loc = Integer.parseInt(st.nextToken());
+			// 1: 북, 2: 남, 3: 서, 4: 동
+			dir[i] = Integer.parseInt(st.nextToken());
+			dist[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int[] result = new int[n];
+		
+		int one=0, two=0, dongDist = 0;
+		for (int i = n; i >=0; i--) {
+			int tmp = 0;
 			
-			if(dir == 1) {
-				// idx 0 ~ w-1
-				tmp = loc;
+			// 핵심 : 한 줄로 펼쳐졌다고 생각!
+			if (dir[i] == 1) {
+				tmp = dist[i];
 			}
-			else if(dir == 4) {
-				// idx w ~ w+h-1
-				tmp = w + loc;
+			else if(dir[i] == 4) {
+				tmp = w + dist[i];
 			}
-			else if(dir == 2) {
-				// idx w+h ~ 2w+h-1
-				tmp = w + h + (w - loc);
+			else if(dir[i] == 2) {
+				tmp = w + h + (w - dist[i]);  // 기준점 유의
 			}
 			else {
-				// idx 2w+h ~ 2w+2h-1
-				tmp = w + h + w + (h - loc);
+				tmp = w + h + w + (h - dist[i]);  // 기준점 유의
 			}
 			
-			map[i] = tmp;
+			if(i==n) dongDist = tmp;
+			
+			// 동근이가 아닐 때는 거리 계산
+			if(i<n) {
+				one = Math.abs(dongDist-tmp);  // 첫번째 방향
+				two = 2*w + 2*h - one;  // 두번째 방향
+				
+				result[i] = one < two ? one : two;  // 더 작은 값으로 저장
+			}
 		}
 		
 		int sum = 0;
-		for(int i=0; i<N; i++) {
-			// 각 상점과 동근이의 차이 중 최솟값을 합
-			sum += Math.min(Math.abs(map[N]-map[i]), 2*(w+h)-Math.abs(map[N]-map[i]));
+		for(int r : result) {
+			sum += r;
 		}
-		
-		System.out.println(sum);
+		sb.append(sum).append("\n");
+		System.out.println(sb);
 	}
 
 }
