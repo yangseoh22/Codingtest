5fba46b7d817cb5a62090ba7a69b1cc0d3cfa9f4
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-20 02:23:03 +0900

    [Silver II] Title: 나무 자르기, Time: 456 ms, Memory: 167852 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,49 +1,46 @@
+import java.util.*;
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());  //나무의 수
-        int m = Integer.parseInt(st.nextToken());  //가져갈 나무의 길이
-        int[] tree = new int[n];  //나무의 길이들을 저장
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int maxT = Integer.MIN_VALUE;
+		int[] tree = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			tree[i] = Integer.parseInt(st.nextToken());
+			maxT = Math.max(tree[i], maxT); 
+		}
+		
+		int low = 0;
+		int high = maxT;
+		int result = maxT;
+		while(low<=high) {
+			long sumTree = 0;
+			int H = (low + high)/2;  // 이분탐색
+			for(int t : tree) {
+				if(t >= H) {
+					sumTree += t - H;					
+				}
+			}
+			
+			if(sumTree < M) {
+				high = H - 1;
+			}
+			else if(sumTree >=M) {
+				low = H + 1;
+				result = H;
+			}
+			
+		}
 
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){  //각 나무의 길이 입력 받기
-            tree[i] = Integer.parseInt(st.nextToken());
-        }
+		System.out.println(result);
+	}
 
-        int start = 0;  //검사할 부분의 왼쪽 끝
-        int end = Arrays.stream(tree).max().getAsInt();  //검사할 부분의 오른쪽 끝
-        
-        int cutter = 0;  //절단기의 높이
-        while(start <= end){
-            long total = 0;  //가져갈 수 있는 나무의 수
-            int mid = (start + end)/2;
-
-            for(int t : tree){
-                //나무가 절단기의 높이 보다 길다면 나무를 잘라 총 합계에 합산
-                if(t>mid){
-                    total += t - mid;
-                }
-            }
-
-            //가져가려던 길이 보다 적게 나왔다면 기준을 mid 왼쪽으로 이동
-            if(total < m){
-                end = mid - 1;
-            }
-            else{  //가져가려던 길이 보다 많이 나왔다면 기준을 mid 오른쪽으로 이동
-                cutter = mid;
-                start = mid + 1;
-            }
-        }
-
-        bw.write(Integer.toString(cutter)+"\n");
-        bw.flush();
-        bw.close();
-    }
 }
