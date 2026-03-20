460887232e47f2d27f84d99bd8b1d06d6833d1dd
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-20 17:46:23 +0900

    [Silver II] Title: 부분수열의 합, Time: 604 ms, Memory: 283380 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,47 +1,57 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int[] arr;
-	static int N, S, cnt;
-
+	static int N, S, result;
+	static int[] nums;
+	static boolean[] selected;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-
+		
 		N = Integer.parseInt(st.nextToken());
 		S = Integer.parseInt(st.nextToken());
-
-		arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-
-		cnt = 0;
-		backtracking(0, 0);
-
-		// 부분 수열의 길이를 전달
-		sb.append(cnt);
-
-		System.out.println(sb);
-	}
-
-	private static void backtracking(int start, int curSum) {
 		
-		// 부분 수열의 크기가 N이 되면 종료
-		if (start == N) return;
-
-		// 조합 구하기
-		for (int i = start; i < N; i++) {
-			if (curSum + arr[i] == S)
-				cnt++;
-
-			backtracking(i + 1, curSum + arr[i]);
+		nums = new int[N];
+		
+		st =new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
-
+		
+		result = 0;
+		selected = new boolean[N];
+		subset(0);
+		
+		System.out.println(result);
 	}
-
+	
+	private static void subset(int cnt) {
+		if(cnt == N) {
+			// 만들어진 부분 집합을 리스트로 생성
+			ArrayList<Integer> tmp = new ArrayList<>();
+			for(int i=0; i<N; i++) {
+				if(selected[i]) {
+					tmp.add(nums[i]);
+				}
+			}
+			
+			// 공집합이 아닐 때만 계산
+			int sum = 0;
+			if(tmp.size()!=0){
+				for(int t : tmp) {
+					sum += t;
+				}
+				if(sum == S) result++;
+			}
+			return;
+		}
+		
+		selected[cnt] = true;
+		subset(cnt + 1);
+		
+		selected[cnt] = false;
+		subset(cnt + 1);
+	}
+	
 }
