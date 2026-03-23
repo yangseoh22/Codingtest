a294a82e63b950a7b379e58cd7d7cd08fcc18cd0
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-03-23 22:34:16 +0900

    [Silver I] Title: 단지번호붙이기, Time: 68 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,10 +1,21 @@
+/*
+ * 시간 : 68ms
+ * 메모리 : 11,540kb
+ * 전략 : DFS
+ * 1. 각 마을별로 번호를 매기기 위해 1을 모두 -1으로 저장
+ * 2. DFS 4방향 탐색으로 방문하지 않았고, -1인 곳이 인접하다면 다 같은 마을로 계산
+ * 3. 해당 탐색을 하면서 마을 개수 계산
+ * 4. 모든 탐색이 끝나면, 해당 배열에 마을 번호마다 몇 개씩 존재 하는지 세서 배열 생성
+ * 5. 오름차순 정렬 후 출력
+ */
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
-	static int N, num;
+	static int N, num, cnt;
 	static int[][] map;
 	static boolean[][] V;
 	public static void main(String[] args) throws Exception {
@@ -25,36 +36,29 @@ public class Main {
 		
 		V = new boolean[N][N];
 		num = 0;
+		ArrayList<Integer> result = new ArrayList<>();
 		for(int r=0; r<N; r++) {
 			for(int c=0; c<N; c++) {
-				if(map[r][c]==-1) {
+				if(map[r][c]==-1 && !V[r][c]) {
 					num++;
+					cnt = 0;
 					dfs(r, c);
+					result.add(cnt);
 				}
 			}
 		}
 		
 		sb.append(num).append("\n");
-		int[] tmp = new int[num + 1];
-		for(int r=0; r<N; r++) {
-			for(int c=0; c<N; c++) {
-				if(map[r][c] != 0) {
-					tmp[map[r][c]]++;
-				}
-			}
+		Collections.sort(result);
+		for(int r : result) {
+			sb.append(r).append("\n");
 		}
-		
-		Arrays.sort(tmp);
-		for(int i=0; i<num; i++) {
-			sb.append(tmp[i+1]).append("\n");
-		}
-		
 		System.out.println(sb);
 	}
 	private static void dfs(int r, int c) {
 		
-		map[r][c] = num;
 		V[r][c] = true;
+		cnt++;
 		
 		for(int i=0; i<4; i++) {
 			int nr = r + dr[i];
