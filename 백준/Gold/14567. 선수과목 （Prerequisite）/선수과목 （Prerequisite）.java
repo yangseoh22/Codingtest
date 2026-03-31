a8c6c4c2b92cbd1fb92e9e4fb951b14a8b571d0c
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-04-01 01:05:28 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 504 ms, Memory: 130824 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M, semester;
+	static ArrayList<Integer>[] graph;
+	static int[] degree;
+	static int[] visited;
+	static Queue<int[]> q = new LinkedList<>();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		graph = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<>();
+		}
+
+		degree = new int[N + 1];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+
+			graph[s].add(e);
+			degree[e]++;
+		}
+
+		semester = 1;
+		for (int i = 1; i <= N; i++) {
+			if (degree[i] == 0) {
+				q.add(new int[] { i, semester});
+			}
+		}
+
+		visited = new int[N + 1];
+		while (!q.isEmpty()) {
+			int[] now = q.poll();
+			visited[now[0]] = now[1];
+			for (int next : graph[now[0]]) {
+				if (visited[next] == 0) { // 방문하지 않은 노드라면
+					degree[next]--;
+					
+					if(degree[next] == 0) {
+						q.add(new int[] {next, now[1]+1});
+					}
+				}
+			}
+		}
+
+		for (int i = 1; i <= N; i++) {
+			sb.append(visited[i]).append(" ");
+		}
+		System.out.println(sb);
+	}
+
+}
