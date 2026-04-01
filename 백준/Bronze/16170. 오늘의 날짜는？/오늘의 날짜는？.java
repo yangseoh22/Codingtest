1ebbb09f926821750e795531553462f0b51f793f
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-04-01 15:49:05 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 60 ms, Memory: 11300 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("2026").append("\n");
+		sb.append("04").append("\n");
+		sb.append("01");
+		System.out.println(sb);
+	}
+
+}
