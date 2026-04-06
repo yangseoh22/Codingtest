3455185d6e7be1c4093f80964af29606e7a853f0
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-04-06 17:35:42 +0900

    [Bronze II] Title: 피보나치 수, Time: 68 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,21 +1,30 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-
+	static int[] memo;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] dp = new int[N+1];
+		memo = new int[N+1];
+		Arrays.fill(memo, -1);
 		
-		dp[1] = 1;
-		if(N>=2) dp[2] = 1;
+		int result = fibo(N);
 		
-		for(int i=3; i<=N; i++) {
-			dp[i] = dp[i-1] + dp[i-2];
+		System.out.println(result);
+	}
+
+	private static int fibo(int n) {
+		if(memo[n]!=-1) return memo[n];
+		
+		if(n==0) return 0;
+		else if(n==1 || n==2) return 1;
+		else {
+			memo[n] = fibo(n-1) + fibo(n-2);
 		}
-		
-		System.out.println(dp[N]);
+	
+		return memo[n];	
 	}
 
 }
