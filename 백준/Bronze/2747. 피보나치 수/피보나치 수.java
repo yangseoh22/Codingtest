39b11aabe7c0f20963a8f5e73ce667aa8c59ef6d
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-04-06 17:36:52 +0900

    [Bronze II] Title: 피보나치 수, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -18,8 +18,7 @@ public class Main {
 	private static int fibo(int n) {
 		if(memo[n]!=-1) return memo[n];
 		
-		if(n==0) return 0;
-		else if(n==1 || n==2) return 1;
+		if(n==1 || n==2) return 1;
 		else {
 			memo[n] = fibo(n-1) + fibo(n-2);
 		}
