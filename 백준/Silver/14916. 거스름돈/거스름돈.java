eee55ad68756b379268c8024a644672016d26f9b
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-04-07 14:59:30 +0900

    [Silver V] Title: 거스름돈, Time: 80 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+	static int[] dp;
+	static int cnt;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		dp = new int[n+1];
+		Arrays.fill(dp, -1);
+		cnt = -1;
+		
+		int result = solve(n, 0);
+		
+		
+		System.out.println(result);
+	}
+
+	private static int solve(int n, int c) {
+		if(dp[n] != -1) return dp[n];
+				
+		if(n<=1) {
+			if(n==0) {
+				cnt = c;
+			}
+			dp[n] = 0;
+		}
+		else if(n==4 || n==3 || n==2) {
+			dp[n] = solve(n-2, c+1);
+		}
+		else {
+			dp[n] = solve(n-5, c+1) + solve(n-2, c+1);
+		}
+		
+		return cnt;
+	}
+
+}
