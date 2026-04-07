bb4c2011035b08603653dc6c9f9eb970f89ee169
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-04-07 15:23:38 +0900

    [Silver V] Title: 거스름돈, Time: 88 ms, Memory: 13916 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -3,36 +3,32 @@ import java.util.Arrays;
 
 public class Main {
 	static int[] dp;
-	static int minCnt = Integer.MAX_VALUE;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int n = Integer.parseInt(br.readLine());
-		dp = new int[n+1];
+		dp = new int[n + 1];
 		Arrays.fill(dp, -1);
-		
-		int result = solve(n, 0);
-		
-		System.out.println(result==Integer.MAX_VALUE?-1:result);
+
+		int result = solve(n);
+
+		System.out.println(result == 100001 ? -1 : result);
 	}
 
-	private static int solve(int n, int c) {
-		if(dp[n] != -1) return dp[n];
-				
-		if(n<=1) {
-			if(n==0) {
-				minCnt = Math.min(minCnt, c);
-			}
-			dp[n] = 0;
-		}
-		else if(n==4 || n==3 || n==2) {
-			dp[n] = solve(n-2, c+1);
-		}
-		else {
-			dp[n] = solve(n-5, c+1) + solve(n-2, c+1);
-		}
+	private static int solve(int n) {
+		if (n == 0) return 0;
+		if (n < 0) return 100001;
+
+		// 메모이제이션
+		if (dp[n] != -1) return dp[n];
 		
-		return minCnt;
+		// 5원으로 하는 경우, 2원으로 하는 경우 중 더 적은 횟수 저장
+		int cnt = Math.min(solve(n - 5), solve(n - 2));
+		
+		if(cnt==100001) return dp[n] = cnt;
+
+		return dp[n] = cnt + 1;
 	}
 
 }
