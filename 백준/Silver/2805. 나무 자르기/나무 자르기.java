1b947e91b5750f0ef234c2ddc31a9d0c18e3d1a5
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-04-08 09:44:03 +0900

    [Silver II] Title: 나무 자르기, Time: 456 ms, Memory: 167780 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -10,37 +10,37 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int maxT = Integer.MIN_VALUE;
 		int[] tree = new int[N];
+		int maxH = Integer.MIN_VALUE;
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
 			tree[i] = Integer.parseInt(st.nextToken());
-			maxT = Math.max(tree[i], maxT); 
+			maxH = Math.max(maxH, tree[i]);
 		}
 		
-		int low = 0;
-		int high = maxT;
-		int result = maxT;
-		while(low<=high) {
-			long sumTree = 0;
-			int H = (low + high)/2;  // 이분탐색
+		int down = 0;
+		int up = maxH;
+		int res = -1;
+		
+		while(down<=up) {
+			int H = (down+up)/2;
+			long sum = 0;
 			for(int t : tree) {
-				if(t >= H) {
-					sumTree += t - H;					
+				if(t>H) {
+					sum += t-H;
 				}
 			}
-			
-			if(sumTree < M) {
-				high = H - 1;
-			}
-			else if(sumTree >=M) {
-				low = H + 1;
-				result = H;
-			}
-			
-		}
 
-		System.out.println(result);
+			if(sum>=M) {
+				res = H;
+				down = H + 1;
+			}
+			else if(sum<M) {
+				up = H - 1;
+			}
+		}
+		
+		System.out.println(res);
 	}
 
 }
