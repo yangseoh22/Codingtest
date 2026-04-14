af16708c7e4f70f5f0632f97ccb22708ff5b2438
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-04-14 12:03:43 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1704 ms, Memory: 349156 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,78 +1,63 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static class Edge implements Comparable<Edge>{
-		int to, weight;
-		public Edge(int to, int weight) {
-			this.to = to;
-			this.weight = weight;
-		}
-		
-		@Override
-		public int compareTo(Edge e) {
-			return this.weight - e.weight;
-		}
-	}
+	static ArrayList<int[]>[] graph;
+	static PriorityQueue<int[]> pq;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		int X = Integer.parseInt(st.nextToken());
 		
-		List<Edge>[] graph = new ArrayList[N+1];
+		graph = new ArrayList[N+1];
 		for(int i=1; i<=N; i++) {
 			graph[i] = new ArrayList<>();
 		}
+		int[] dist = new int[N+1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
 		
-		for (int i = 0; i < M; i++) {
+		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-
-			graph[A].add(new Edge(B, 1));  // 가중치 없는 그래프라 모두 1
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			
+			graph[from].add(new int[] {to, 1});
 		}
 
-		int[] dist = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			dist[i] = (int) 1e9;
-		}
-		
-		dist[X] = 0;  // 시작점
-		pq.add(new Edge(X, 0));
+		pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+		pq.add(new int[] {X, 0});
+		dist[X] = 0;
 		
 		while(!pq.isEmpty()) {
-			Edge cur = pq.poll();
-			
-			int minIdx = cur.to;
-			int minDist = cur.weight;
-			
-			if(minDist != dist[minIdx]) continue;
+			int[] now = pq.poll();
 
-			// 인접 노드 확인
-			for(Edge next : graph[minIdx]) {
-				// 다음에 가려는 곳의 거리가 현재 노드를 거쳐 가는 것보다 크다면 갱신
-				if(dist[next.to] > dist[minIdx]+next.weight) {
-					dist[next.to] = dist[minIdx]+next.weight;
-					pq.add(new Edge(next.to, dist[next.to]));
+			if(now[1] > dist[now[0]]) continue;
+			
+			for(int[] next : graph[now[0]]) {
+				int newDist = dist[now[0]] + next[1];
+				if(dist[next[0]]>newDist) {
+					dist[next[0]] = newDist;
+					pq.add(new int[] {next[0], newDist});
 				}
 			}
 		}
-
-		boolean flag = false;
-		for (int i = 1; i <= N; i++) {
-			if (dist[i] == K) {
-				flag = true;
+		
+		boolean find = false;
+		for(int i=1; i<=N; i++) {
+			if(dist[i] == K) {
+				find = true;
 				sb.append(i).append("\n");
 			}
 		}
-		if(!flag) System.out.println(-1);
-		else System.out.println(sb);
+		
+		if(!find) sb.append(-1);
+		
+		System.out.println(sb);
 	}
 
 }
