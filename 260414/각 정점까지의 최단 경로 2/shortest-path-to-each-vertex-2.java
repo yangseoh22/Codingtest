cf94a9e8de5d38f45aae5817eb9b3bc7d3fbaf8c
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-04-14 10:34:49 +0900

    Title: 각 정점까지의 최단 경로 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260414/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[][] adj = new int[N+1][N+1];
+		for(int i=1; i<=N; i++) {
+			Arrays.fill(adj[i], 100001);
+		}
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			adj[start][end] = weight;  // 방향 그래프 -> 행렬
+		}
+		
+		for(int i=1; i<=N; i++) {
+			adj[i][i] = 0;
+		}
+		
+		for(int k=1; k<=N; k++) {
+			for(int i=1; i<=N; i++) {
+				for(int j=1; j<=N; j++) {
+					adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
+				}
+			}
+		}
+		
+		for(int r=1; r<=N; r++) {
+			for(int c=1; c<=N; c++) {
+				if(adj[r][c]==100001) sb.append(-1).append(" ");
+				else sb.append(adj[r][c]).append(" ");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+}
