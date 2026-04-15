b4bef191e32da47965abaf32c58ba3a3317cf051
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-04-15 09:18:33 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,079 ms, Memory: 103,692 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -2,71 +2,71 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-	static ArrayList<Integer>[] taller;
-	static ArrayList<Integer>[] shorter;
-	static int N, M;
+	static final int MAX = 1000000;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		
+
 		int T = Integer.parseInt(br.readLine());
-		for(int t=1; t<=T; t++) {
+		for (int t = 1; t <= T; t++) {
 			sb.append("#").append(t).append(" ");
-			
-			N = Integer.parseInt(br.readLine());
-			M = Integer.parseInt(br.readLine());
-			
-			taller = new ArrayList[N+1];
-			shorter = new ArrayList[N+1];
-			for(int i=1; i<=N; i++) {
-				taller[i] = new ArrayList<>();
-				shorter[i] = new ArrayList<>();
+
+			int N = Integer.parseInt(br.readLine());
+			int M = Integer.parseInt(br.readLine());
+
+			int[][] taller = new int[N + 1][N + 1];
+			int[][] shorter = new int[N + 1][N + 1];
+
+			// 거리 배열 초기화
+			for (int r = 1; r <= N; r++) {
+				for (int c = 1; c <= N; c++) {
+					taller[r][c] = MAX;
+					shorter[r][c] = MAX;
+				}
+				taller[r][r] = 0;
+				shorter[r][r] = 0;
 			}
-			
-			for(int i=0; i<M; i++) {
+
+			// 연결 내용 추가
+			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
-				
-				taller[from].add(to);
-				shorter[to].add(from);
+
+				taller[from][to] = 1;
+				shorter[to][from] = 1;
 			}
 			
-			int tallCnt = 0;  // 나보다 큰 사람 수
-			int shortCnt = 0;  // 나보다 작은 사람 수
-			int result = 0;  // 나의 위치를 아는 사람 수
-			for(int i=1; i<=N; i++) {
-				tallCnt = BFS(i, taller);
-				shortCnt = BFS(i, shorter);
-				
-				if(tallCnt + shortCnt == N-1) result++;
+			// 플로이드 워셜로 연결된 노드 표시
+			for (int k = 1; k <= N; k++) {
+				for (int i = 1; i <= N; i++) {
+					for (int j = 1; j <= N; j++) {
+						taller[i][j] = Math.min(taller[i][j], taller[i][k] + taller[k][j]);
+						shorter[i][j] = Math.min(shorter[i][j], shorter[i][k] + shorter[k][j]);
+					}
+				}
 			}
 			
+			// 두 배열을 같이 봤을 때 각 행에 대해 둘 다 MAX인 곳이 없으면 자신의 키를 알 수 있음
+			int result = 0;
+			for (int r = 1; r <= N; r++) {
+				boolean flag = false;
+				for (int c = 1; c <= N; c++) {
+					if (taller[r][c] != MAX || shorter[r][c] != MAX) {
+						flag = true;
+					}
+					else {
+						flag = false;
+						break;
+					}
+				}
+				if(flag) result++;
+			}
 			sb.append(result).append("\n");
 		}
 		System.out.println(sb);
-	}
-	private static int BFS(int start, ArrayList<Integer>[] graph) {
-		Queue<Integer> q = new LinkedList<>();
-		boolean[] visited = new boolean[N+1];
-		q.add(start);
-		visited[start] = true;
-		
-		int cnt = 0;
-		while(!q.isEmpty()) {
-			int now = q.poll();
-			
-			for(int next : graph[now]) {
-				if(!visited[next]) {
-					visited[next] = true;
-					q.add(next);
-					cnt++;
-				}
-			}
-		}
-		
-		return cnt;
 	}
 
 }
