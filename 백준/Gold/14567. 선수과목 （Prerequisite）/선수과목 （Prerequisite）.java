07cdbb5f69dc90913cfda800d9c7ed3c9300889e
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-04-21 21:33:52 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 500 ms, Memory: 131128 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -1,58 +1,59 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N, M, semester;
 	static ArrayList<Integer>[] graph;
 	static int[] degree;
-	static Queue<Integer> q = new LinkedList<>();
-	static int[] s;
+	static int[] visited;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		graph = new ArrayList[N + 1];
-		for (int i = 1; i <= N; i++) {
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		graph = new ArrayList[N+1];
+		for(int i=1; i<=N; i++) {
 			graph[i] = new ArrayList<>();
 		}
-
-		degree = new int[N + 1];
-		for (int i = 0; i < M; i++) {
+		
+		degree = new int[N+1];
+		visited = new int[N+1];
+		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
-
+			
 			graph[s].add(e);
 			degree[e]++;
 		}
 		
-		s = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			if (degree[i] == 0) {
+		Queue<Integer> q = new LinkedList<>();
+		for(int i=1; i<=N; i++) {
+			if(degree[i] == 0) {
+				visited[i] = 1;
 				q.add(i);
-				s[i] = 1;  // 학기 수 저장
 			}
 		}
 		
-		while (!q.isEmpty()) {
+		while(!q.isEmpty()) {
 			int now = q.poll();
 			
-			for (int next : graph[now]) {
-				degree[next]--;
-
-				if (degree[next] == 0) {
-					q.add(next);
-					s[next] = s[now] + 1;
+			for(int next : graph[now]){
+				if(visited[next] == 0) {
+					degree[next]--;
+					
+					if(degree[next] == 0) {
+						visited[next] = visited[now] + 1;
+						q.add(next);
+					}
 				}
 			}
 		}
-
-		for (int i = 1; i <= N; i++) {
-			sb.append(s[i]).append(" ");
+		
+		for(int i=1; i<=N; i++) {
+			sb.append(visited[i]).append(" ");
 		}
 		System.out.println(sb);
 	}
