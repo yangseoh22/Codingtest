29752312b6e07a92bc8ae99aebd01f3115d7950f
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-04-28 21:32:41 +0900

    [D2] Title: 초심자의 회문 검사, Time: 76 ms, Memory: 24,960 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -1,30 +1,27 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Solution {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		
-		int T = Integer.parseInt(br.readLine());
-		for(int t=1; t<=T; t++) {
-			sb.append("#").append(t).append(" ");
-			char[] word = br.readLine().toCharArray();
-			
-			int idx = word.length-1;
+		int TC = Integer.parseInt(br.readLine());
+		for(int t=1; t<=TC; t++) {
+			String tmp = br.readLine();
+			char[] word = tmp.toCharArray();
+			int left = 0;
+			int right = word.length - 1;
 			boolean flag = true;
-			for(int i=0; i<word.length/2; i++) {
-				if(word[i] != word[idx--]) {
+			while(left<right) {
+				if(word[left++] != word[right--]) {
 					flag = false;
 					break;
 				}
 			}
 			
-			if(flag) sb.append(1).append("\n");
-			else sb.append(0).append("\n");
+			sb.append("#").append(t).append(" ").append(flag?1:0).append("\n");
 		}
 		System.out.println(sb);
 	}
