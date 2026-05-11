eeb327b29460c23d7a24a01b1fadd583d9921575
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-05-11 13:30:43 +0900

    [D2] Title: 나무 높이, Time: 86 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,8 +1,7 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -10,39 +9,41 @@ public class Solution {
 		
 		int TC = Integer.parseInt(br.readLine());
 		for(int t=1; t<=TC; t++) {
-			int N = Integer.parseInt(br.readLine());
 			
+			int N = Integer.parseInt(br.readLine());
 			int[] trees = new int[N];
-			int maxH = Integer.MIN_VALUE;
+			
+			int maxT = Integer.MIN_VALUE;
 			st = new StringTokenizer(br.readLine());
 			for(int i=0; i<N; i++) {
 				trees[i] = Integer.parseInt(st.nextToken());
-				maxH = Math.max(trees[i], maxH);
+				maxT = Math.max(maxT, trees[i]);  // 최대 높이
 			}
 			
-			int one = 0;
 			int two = 0;
-			for(int tr : trees) {
-				int diff = maxH - tr;
+			int one = 0;
+			for(int i=0; i<N; i++) {
+				int diff = maxT - trees[i];
 				
 				two += diff/2;
 				one += diff%2;
 			}
 			
-			// 재분배
 			while(one+1<two) {
 				two -= 1;
 				one += 2;
 			}
 			
 			int days = 0;
-			if(one>two) days = one * 2 - 1;
-			else {
-				days = two * 2;
+			if(one>two) {
+				days = one*2 - 1;
+			} else {
+				days = two*2;
 			}
 			
 			sb.append("#").append(t).append(" ").append(days).append("\n");
 		}
+		
 		System.out.println(sb);
 	}
 
