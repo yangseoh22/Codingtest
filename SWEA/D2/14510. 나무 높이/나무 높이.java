b378857d4b5ba125eff547467523ddddcf34c6b3
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-05-14 09:42:13 +0900

    [D2] Title: 나무 높이, Time: 91 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,45 +1,48 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Solution{
- 	public static void main (String[] args) throws Exception {
-     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        
-        int TC = Integer.parseInt(br.readLine());
-        for(int t=1; t<=TC; t++){
-        	int N = Integer.parseInt(br.readLine());
-            
-            st = new StringTokenizer(br.readLine());
-            int[] tree = new int[N];
-            int maxH = Integer.MIN_VALUE;
-            for(int i=0; i<N; i++){
-                tree[i] = Integer.parseInt(st.nextToken());
-                maxH = Math.max(maxH, tree[i]);
-            }
-            
-            int one = 0;
-            int two = 0;
-            for(int i=0; i<N; i++){
-            	int diff = maxH - tree[i];
-                
-                two += diff/2;
-                one += diff%2;
-            }
-            
-            while(two - one > 1){
-            	two -= 1;
-                one += 2;
-            }
-            
-            int day = 0;
-            if(one > two) day = one * 2 - 1;
-            else day = two * 2;
-            
-            sb.append("#").append(t).append(" ").append(day).append("\n");
-        }
-        
-        System.out.println(sb);
-    } 
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int TC = Integer.parseInt(br.readLine());
+		
+		for(int t=1; t<=TC; t++) {
+			int N = Integer.parseInt(br.readLine());
+			
+			int[] tree = new int[N];
+			st = new StringTokenizer(br.readLine());
+			int maxH = Integer.MIN_VALUE;
+			for(int i=0; i<N; i++) {
+				tree[i] = Integer.parseInt(st.nextToken());
+				maxH = Math.max(maxH, tree[i]);
+			}
+			
+			int one = 0;
+			int two = 0;
+			for(int i=0; i<N; i++) {
+				int diff = maxH - tree[i];
+				
+				two += diff/2;
+				one += diff%2;
+			}
+			
+			while(two-one > 1) {
+				two -= 1;
+				one += 2;
+			}
+			
+			int day = 0;
+			if(one > two) day = one * 2 - 1;
+			else day = two * 2;
+			
+			sb.append("#").append(t).append(" ").append(day).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
 }
