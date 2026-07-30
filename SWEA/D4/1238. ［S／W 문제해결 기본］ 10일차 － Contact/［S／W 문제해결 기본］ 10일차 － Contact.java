d4290bedc2d326619c61b3117cc0d6d4196467a3
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-07-30 17:19:10 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 90 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -2,71 +2,63 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    static int N;
     static ArrayList<Integer>[] graph;
-    static int[] visited;
-    static int maxNum;
-    public static void main(String[] args) throws Exception {
+    static int visited[];
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        int T = 10;
-        for(int t=1; t<=T; t++){
+        int TC = 10;
+        for(int t=1; t<=TC; t++){
+            graph = new ArrayList[101];
+            for(int i=0; i<101; i++){
+                graph[i] = new ArrayList<>();
+            }
+            visited = new int[101];
+
             st = new StringTokenizer(br.readLine());
             int len = Integer.parseInt(st.nextToken());
             int start = Integer.parseInt(st.nextToken());
 
-            graph = new ArrayList[101];
-            for(int i=1; i<101; i++){
-                graph[i] = new ArrayList<>();
-            }
-
-            visited = new int[101];
-            Arrays.fill(visited, -1);
-
+            // 연락 그래프 생성
             st = new StringTokenizer(br.readLine());
             for(int i=0; i<len/2; i++){
                 int from = Integer.parseInt(st.nextToken());
                 int to = Integer.parseInt(st.nextToken());
-
                 graph[from].add(to);
             }
 
-            maxNum = start;
-            bfs(start);
+            BFS(start);
 
             int maxDepth = 0;
+            int result = start;
             for(int i=0; i<101; i++){
-                if(maxDepth<visited[i]){
-                    maxNum = i;
+                if(maxDepth <= visited[i]){
+                    result = i;
                     maxDepth = visited[i];
-                }
-                else if(maxDepth==visited[i]){
-                    maxNum = Math.max(maxNum, i);
                 }
             }
 
-            sb.append("#").append(t).append(" ").append(maxNum).append("\n");
+            sb.append("#").append(t).append(" ").append(result).append("\n");
         }
         System.out.println(sb);
     }
 
-    private static void bfs(int start) {
-        Queue<Integer> q = new LinkedList<>();
+    private static void BFS(int start) {
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(start);
         visited[start] = 0;
 
         while(!q.isEmpty()){
-            int now = q.poll();
+            int curr = q.poll();
 
-            for(int next : graph[now]){
-                if(visited[next] == -1){
-                    visited[next] = visited[now] + 1;
+            for(int next : graph[curr]){
+                if(visited[next] == 0){  // 방문하지 않은 곳이라면
+                    visited[next] = visited[curr] + 1;
                     q.add(next);
                 }
             }
         }
     }
-
 }
