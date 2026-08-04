83a4e9cd2635fcab5f5ceb59c14ed73d206ef2ff
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-08-04 19:55:36 +0900

    [D3] Title: 햄버거 다이어트, Time: 137 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,85 +1,57 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Solution {
-	public static class List{
-		int score, cal;
-		public List(int score, int cal) {
-			this.score = score;
-			this.cal = cal;
-		}
-	}
-	
-	static int N, L;
-	static boolean[] selected;
-	static List[] hambug;
-	static ArrayList<Integer> choice;
-	static int maxSum;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int TC = Integer.parseInt(br.readLine());
-		
-		for(int t=1; t<=TC; t++) {
-			sb.append("#").append(t).append(" ");
-			
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			L = Integer.parseInt(st.nextToken());
-			
-			hambug = new List[N];
-			for(int i=0; i<N; i++) {
-				st = new StringTokenizer(br.readLine());				
-				int score = Integer.parseInt(st.nextToken());
-				int cal = Integer.parseInt(st.nextToken());
-				
-				hambug[i] = new List(score, cal);
-			}
-			
-			selected = new boolean[N];
-			choice = new ArrayList<>();
-			maxSum = Integer.MIN_VALUE;
-			combi(0);
-			
-			
-			sb.append(maxSum).append("\n");
-		}
-		System.out.println(sb);
-	}
-	private static void combi(int cnt) {
-		if(cnt == N) {
-			for(int i=0; i<N; i++) {
-				if(selected[i]) choice.add(i);
-			}
-			if(choice.size()!=0) calScore(choice);
-			choice = new ArrayList<>();  // 초기화 필수!!!
-			return;
-		}
-		
-		selected[cnt] = true;
-		combi(cnt + 1);
-		
-		selected[cnt] = false;
-		combi(cnt + 1);
-	}
-	
-	private static void calScore(ArrayList<Integer> arr) {
-		int calSum = 0;
-		int scSum = 0;
-		
-		for(int i=0 ; i<arr.size(); i++) {
-			int idx = arr.get(i);
-			
-			calSum += hambug[idx].cal;
-			scSum += hambug[idx].score;
-			
-			if(calSum>L) return;  // 제한 칼로리 넘으면 무시
-		}
-		
-		// 조합의 칼로리가 L을 넘기지 않으면 갱신
-		maxSum = Math.max(scSum, maxSum);
-	}
+    static int L, N;
+    static int maxScore;
+    static int[][] menu;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for(int t=1; t<=T; t++){
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            L = Integer.parseInt(st.nextToken());
+
+            menu = new int[N][2];
+            for(int i=0; i<N; i++){
+                st = new StringTokenizer(br.readLine());
+                // 메뉴별 점수와 칼로리
+                menu[i][0] = Integer.parseInt(st.nextToken());  // 점수
+                menu[i][1] = Integer.parseInt(st.nextToken());  // 칼로리
+            }
+
+            // 최대 점수
+            maxScore = 0;
+
+            // 메뉴 조합 생성
+            findCombi(0, 0, 0);
+
+            sb.append("#").append(t).append(" ").append(maxScore).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static void findCombi(int idx, int score, int cal) {
+        // 탐색 종료
+        if (cal > L) {
+            return;
+        }
+
+        // 모든 재료를 다 확인한 경우
+        if (idx == N) {
+            maxScore = Math.max(maxScore, score);
+            return;
+        }
+
+        // 선택하는 경우
+        findCombi(idx + 1, score + menu[idx][0], cal + menu[idx][1]);
+
+        // 선택하지 않는 경우
+        findCombi(idx + 1, score, cal);
+    }
+
 }
