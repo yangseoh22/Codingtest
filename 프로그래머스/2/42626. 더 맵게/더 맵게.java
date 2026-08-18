787a562076cac295b042f631ca1b7a3a089ed69e
yangseoh22 <149481688+yangseoh22@users.noreply.github.com>
2026-08-18 17:53:40 +0900

    [level 2] Title: 더 맵게, Time: 668.33 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -10,26 +10,14 @@ class Solution {
             pq.add(scoville[i]);
         }
         
-        while(pq.size()>1){
-            if(upperK(K)){
-                return cnt;
-            } else{
-                pq.add(pq.poll() + pq.poll()*2);
-                cnt++;
-            }
+        // 큐에 남은 요소가 1개 초과면서, 가장 작은 수가 K미만이면 반복
+        while(pq.size() > 1 && pq.peek() < K){
+            pq.add(pq.poll() + pq.poll()*2);
+            cnt++;
         }
+        
+        // 남은 하나의 수가 K이상이면 횟수, 아니면 -1 반환
         if(pq.peek()>= K) return cnt;
         else return -1;
-    }
-    
-    public boolean upperK(int N){
-        for(int i=0; i<pq.size(); i++){
-            int num = pq.poll();
-            if(num < N){
-                pq.add(num);
-                return false;
-            }
-        }
-        return true;
     }
 }
